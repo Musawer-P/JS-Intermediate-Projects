7de6236adc7fece98aeab7f332708d4eb9eb6dd5
Musawer-P <m.musawer.p@gmail.com>
2025-07-16 23:58:42 +0430

    adding FAQ section on project

--- Netflix-clone/script.js
+++ Netflix-clone/script.js
@@ -3,3 +3,19 @@ document.querySelectorAll('.banner__button').forEach(button => {
     alert(`${button.textContent} button clicked!`);
   });
 });
+
+
+  document.querySelectorAll(".faq-f").forEach((question) => {
+    question.addEventListener("click", () => {
+      const answer = question.nextElementSibling;
+      const icon = question.querySelector("img");
+
+      if (answer.style.display === "none") {
+        answer.style.display = "block";
+        icon.src = "images/minus.png";
+      } else {
+        answer.style.display = "none";
+        icon.src = "images/plus.png";
+      }
+    });
+  });
